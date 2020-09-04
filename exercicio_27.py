ca70d4a429940b4b1b14904e62b11b3f37af962f
kazenski-dev <app.share.adm@gmail.com>
2020-09-04 00:29:12 -0300

    Exercicios 28 ao 33 am desenv.

--- exercicio_27.py
+++ exercicio_27.py
@@ -12,16 +12,13 @@ class Quot:
 
     def traduzir_quot (self, codigo):
 
-        secreto = {0:" ",1:"a",2:"b",3:"c",4:"d",5:"e",6:"f",7:"g",8:"h",9:"i",10:"j",11:"k",12:"l",13:"m",14:"n",15:"o",16:"p",17:"q",18:"r",19:"s",
-                   20:"t",21:"u",22:"v",23:"x",24:"x",25:"y",26:"z"}
+        secreto = {0: " ",1: "a",2: "b",3: "c",4: "d",5: "e",6: "f",7: "g",8: "h",9: "i",10: "j",11: "k",12: "l",13: "m",14: "n",15: "o",16: "p",17: "q",18: "r",19: "s",
+                   20: "t",21: "u",22: "v",23: "x",24: "x",25: "y",26: "z"}
         frase = []
-        valor = []
 
         for a in codigo:
             for b, c in secreto.items():
                 if codigo[a] == secreto[b]:
-                    
-
                     frase.append(secreto[b])
                 else:
                     print("Valor {0} nao encontrado, prosseguindo...".format(codigo[a]))
@@ -32,7 +29,7 @@ mensagem = []
 
 #Main Code
 while validar != 999:
-    for n in range(100):
+    for n in range(3):
         mensagem.append(int(input("Digite um número para adicioná-lo à lista 1: ")))
         validar = int(input("Digite '999' para SAIR e criptografar:"))
         if validar == 999:
---
+++ exercicio_30.py
@@ -0,0 +1,14 @@
+"""
+30 - Crie um dict com 5 nomes e idades, crie um segundo dict duplicando os itens.
+"""
+import copy
+
+#Variables
+nome_idade = {'Eduardo': 29, 'Murilo': 27, 'Alessandra': 30, 'Carlos': 17, 'Ana': 14}
+duplicando = {}
+
+#Main Code
+
+duplicando = copy.deepcopy(nome_idade)
+print(duplicando)
+
